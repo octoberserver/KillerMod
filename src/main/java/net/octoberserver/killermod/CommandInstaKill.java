66d7d7583d1c20e5e3c336e12bb70309c3f24fbc
october1234 <octoberserver@gmail.com>
2024-03-02 23:10:57 +0800

    Initial commit

---
+++ src/main/java/net/octoberserver/killermod/CommandInstaKill.java
@@ -0,0 +1,53 @@
+package net.octoberserver.killermod;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
+
+import javax.annotation.Nullable;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CommandInstaKill extends CommandBase {
+    public CommandInstaKill() {
+    }
+
+    public String getName() {
+        return "instakill";
+    }
+
+    public int getRequiredPermissionLevel() {
+        return 2;
+    }
+
+    public String getUsage(ICommandSender p_71518_1_) {
+        return "commands.kill.usage";
+    }
+
+    public void execute(MinecraftServer server, ICommandSender sender, String[] params) throws CommandException {
+        if (params.length == 0) {
+            EntityPlayer senderPlayer = getCommandSenderAsPlayer(sender);
+            senderPlayer.setHealth(0);
+        } else {
+            EntityPlayer targetPlayer = getPlayer(server, sender, params[0]);
+            targetPlayer.setHealth(0);
+        }
+    }
+
+    public boolean isUsernameIndex(String[] p_82358_1_, int p_82358_2_) {
+        return p_82358_2_ == 0;
+    }
+
+    public List<String> getTabCompletions(MinecraftServer p_184883_1_, ICommandSender p_184883_2_, String[] p_184883_3_, @Nullable BlockPos p_184883_4_) {
+        return p_184883_3_.length == 1 ? getListOfStringsMatchingLastWord(p_184883_3_, p_184883_1_.getOnlinePlayerNames()) : Collections.emptyList();
+    }
+}
---
+++ src/main/java/net/octoberserver/killermod/KillerMod.java
@@ -0,0 +1,39 @@
+package net.octoberserver.killermod;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = KillerMod.MODID, name = KillerMod.NAME, version = KillerMod.VERSION, serverSideOnly = true, acceptableRemoteVersions = "*")
+public class KillerMod {
+    public static final String MODID = "killer_mod";
+    public static final String NAME = "Example Mod";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+    @EventHandler
+    public static void serverInit(FMLServerStartingEvent event) {
+        event.registerServerCommand(new CommandInstaKill());
+    }
+}
